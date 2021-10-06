6294e5f828b56c46026f36b4a95043ffd16459bb
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2021-10-06 03:33:05 +0300

    arrays

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -2,8 +2,19 @@ package arrays
 
 import "fmt"
 
-// const arraySize = 5
+const arraySize = 5
 
-func Name(){
-	fmt.Println("austin")
+func Array(){
+	// array of 5 numbers
+	var nums [arraySize]int
+	fmt.Println(nums)
+
+	// array of strings
+	// shorthand
+	names := [arraySize] string {"apple","facebook","tesla","google","microsoft"}
+	
+	// loop through array and print each
+	for i:=arraySize; i > 0; i-- {
+		fmt.Printf("%s\n", names[i-1])
+	}
 }
--- dsa.go
+++ dsa.go
@@ -3,5 +3,5 @@ package main
 import "dsa/arrays"
 
 func main(){
-	arrays.Name()
+	arrays.Array()
 }
