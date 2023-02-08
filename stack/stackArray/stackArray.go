ef2d1f0f1ec82148e18b7d92eeb561b3cc012e1a
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2023-02-08 09:43:54 +0300

    fix: initialize top of stack with -1

--- stack/stackArray/stackArray.go
+++ stack/stackArray/stackArray.go
@@ -9,19 +9,25 @@ type StackArray[T any] struct {
 	top   int
 }
 
-// push
+// NewStack initialize new Stack
+func NewStack[T any]() *StackArray[T] {
+	stack := new(StackArray[T])
+	stack.top = -1
+	return stack
+}
+
+// Push add node to the top of the stack
 func (s *StackArray[T]) Push(x T) error {
 	// check for stack overflow
 	if s.IsFull() {
 		return errors.New("stack is full")
 	}
-
 	s.top++
 	s.stack[s.top] = x
 	return nil
 }
 
-// pop top node in the stack
+// Pop remove top node in the stack
 func (s *StackArray[T]) Pop() (T, error) {
 	// check for stack underflow
 	if s.IsEmpty() {
@@ -34,7 +40,7 @@ func (s *StackArray[T]) Pop() (T, error) {
 	return topNode, nil
 }
 
-// peek top node in the stack
+// Peek peek top node in the stack
 func (s *StackArray[T]) Peek() (T, error) {
 	// check for stack underflow
 	if s.IsEmpty() {
@@ -43,12 +49,20 @@ func (s *StackArray[T]) Peek() (T, error) {
 	return s.stack[s.top], nil
 }
 
-// isEmpty
+// IsEmpty check if stack is empty
 func (s *StackArray[T]) IsEmpty() bool {
 	return s.top == -1
 }
 
-// isFull
+// IsFull check if stack is full
 func (s *StackArray[T]) IsFull() bool {
 	return s.top >= stackSize
+}
+
+// Print log nodes in the stack
+func (s *StackArray[T]) Print() {
+	// print each node with its index
+	for i := s.top; i >= 0; i-- {
+		println(i, s.stack[i])
+	}
 }
